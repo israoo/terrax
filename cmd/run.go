edb418724f1590458b16f76f099f3c0a551fbcd6
Israel Oña Ordoñez <israoo@outlook.com>
2026-06-25 01:27:12 -0500

    fix: apply plans dir cleanup fix to terrax run command

--- cmd/run.go
+++ cmd/run.go
@@ -59,7 +59,17 @@ func runCommand(cmd *cobra.Command, args []string) error {
 	repoRoot, filterPaths := collectTransitiveDeps(workDir)
 
 	if command == "plan" && (viper.GetBool("plan.summary_enabled") || viper.GetBool("plan.review_enabled")) {
-		_ = os.RemoveAll(filepath.Join(repoRoot, config.DefaultOutputDir))
+		jsonOutDir := viper.GetString("plan.json_out_dir")
+		if jsonOutDir == "" {
+			jsonOutDir = config.DefaultJSONOutDir
+		}
+		var absPlansDir string
+		if filepath.IsAbs(jsonOutDir) {
+			absPlansDir = jsonOutDir
+		} else {
+			absPlansDir = filepath.Join(repoRoot, jsonOutDir)
+		}
+		_ = os.RemoveAll(absPlansDir)
 	}
 
 	groups, err := buildGroupedExecution(filterPaths, repoRoot)
